b29220da49658ee1c8430b3564d4f8d8ed376105
79612 <79612@DESKTOP-O1P5TC5>
2024-02-20 11:05:54 +0300

    Добавьте файлы проекта.

---
+++ перестановка/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace перестановка
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tea = "кофе";
+            string coffee = "чай";
+            string emptyCup;
+            
+            emptyCup = tea;
+            tea = coffee;
+            coffee = emptyCup;
+
+            Console.WriteLine($"в чашке чай:{tea}, в чашке кофе:{coffee}");
+        }
+    }
+}
